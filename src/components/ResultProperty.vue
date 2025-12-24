8fcafeefd99153e69796b4dd3302b2860b29d9f8
Shreenivas M <shreenivas.madagundi@in.pega.com>
2025-12-24 17:02:13 +0530

    Hub Image url issue resolved

--- src/components/ResultProperty.vue
+++ src/components/ResultProperty.vue
@@ -188,7 +188,7 @@ export default {
       }
 
       return this.config.display === 'image'
-        ? `${this.articlePath}/${prop}`.replaceAll(' ', '_')
+        ? `${this.articlePath}${prop}`.replaceAll(' ', '_')
         : false;
     },
     href(prop) {
